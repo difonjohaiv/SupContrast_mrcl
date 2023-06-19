aea76e1426c93e5befd1eac483cfe965558ae60d
difonjohaiv <edison0824@outlook.com>
2023-06-19 15:26:59 +0800

    0

--- losses.py
+++ losses.py
@@ -50,7 +50,7 @@ class SupConLoss(nn.Module):
             labels = labels.contiguous().view(-1, 1)  # contiguous()为了保证内存的连续性，view（-1，1）第一个dim自动推断，第二个dim为1
             if labels.shape[0] != batch_size:
                 raise ValueError('Num of labels does not match num of features')
-            mask = torch.eq(labels, labels.T).float().to(device)  # eq()函数的作用是判读对比双方是否equal，相等就是1，不相等就是0
+            mask = torch.eq(labels, labels.T).float().to(device)  # eq()函数的作用是判读对比双方是否equal，相等就是1，不相等就是0。样本之间是否为相同标签
         else:
             mask = mask.float().to(device)
 
@@ -61,7 +61,7 @@ class SupConLoss(nn.Module):
             anchor_count = 1
         elif self.contrast_mode == 'all':
             anchor_feature = contrast_feature
-            anchor_count = contrast_count
+            anchor_count = contrast_count  # 每个图像都有2个锚节点
         else:
             raise ValueError('Unknown mode: {}'.format(self.contrast_mode))
 
@@ -70,25 +70,25 @@ class SupConLoss(nn.Module):
             torch.matmul(anchor_feature, contrast_feature.T),
             self.temperature)
         # for numerical stability
-        logits_max, _ = torch.max(anchor_dot_contrast, dim=1, keepdim=True)
+        logits_max, _ = torch.max(anchor_dot_contrast, dim=1, keepdim=True)  # 在第二个dim取最大值，并返回最大值和其索引
         logits = anchor_dot_contrast - logits_max.detach()
 
         # tile mask
-        mask = mask.repeat(anchor_count, contrast_count)
-        # mask-out self-contrast cases
-        logits_mask = torch.scatter(
-            torch.ones_like(mask),
-            1,
-            torch.arange(batch_size * anchor_count).view(-1, 1).to(device),
-            0
+        mask = mask.repeat(anchor_count, contrast_count)  # repeat的作用是,沿着指定的维度复制tensor
+        # mask-out self-contrast cases 对角线置0，不计算自身的对比损失
+        logits_mask = torch.scatter(  # 将某个Tensor中的特定索引的值,赋值(scatter)给另一个Tensor指定的索引。
+            torch.ones_like(mask),  # 目标Tensor,被赋值
+            1,  # 沿着哪个维度赋值
+            torch.arange(batch_size * anchor_count).view(-1, 1).to(device),  # 需要赋值的索引
+            0  # 要赋的值
         )
         mask = mask * logits_mask
 
         # compute log_prob
         exp_logits = torch.exp(logits) * logits_mask
-        log_prob = logits - torch.log(exp_logits.sum(1, keepdim=True))
+        log_prob = logits - torch.log(exp_logits.sum(1, keepdim=True))  # 所有样本的对比损失，包括 正样本对 与 负样本对
 
-        # compute mean of log-likelihood over positive
+        # compute mean of log-likelihood over positive正样本对
         mean_log_prob_pos = (mask * log_prob).sum(1) / mask.sum(1)
 
         # loss
